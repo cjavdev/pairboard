69633ee93191213b95d5fc888d15be720f0bcb4f
CJ Avilla <cjavilla@gmail.com>
2014-07-28 13:47:28 -0700

    add spec for a in folding_cipher

--- spec/03b_spec.rb
+++ spec/03b_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 require '03b'
 
 describe 'folding_cipher' do
+  it 'encrypts the a correctly' do
+    expect(folding_cipher("a")).to eq("z")
+    expect(folding_cipher_solution("a")).to eq("z")
+  end
+
   it 'encrypts the plaintext correctly' do
     expect(folding_cipher("thisisatest")).to eq("gsrhrhzgvhg")
     expect(folding_cipher_solution("thisisatest")).to eq("gsrhrhzgvhg")
